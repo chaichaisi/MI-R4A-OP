33e2ad3900b629628cdcc9d7e3eae56cb84b0495
WillowSauceR <63277109+WillowSauceR@users.noreply.github.com>
2021-12-31 17:08:55 +0800

    Update R4A.sh

--- MI-R4A/R4A.sh
+++ MI-R4A/R4A.sh
@@ -20,7 +20,7 @@
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='R4A-G'' package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i 's/OpenWrt /Compiled time $(TZ=UTC-8 date "+%Y.%m.%d-%H:%M:%S") @ WillowSauceR /g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/OpenWrt /Build time $(TZ=UTC-8 date "+%Y.%m.%d-%H:%M:%S") @ WillowSauceR /g' package/lean/default-settings/files/zzz-default-settings
 
 #更改主机型号，支持中文。 
 ##sed -i "s/Xiaomi Mi Router 4A Gigabit Edition/Xiaomi Mi Router 4A Gigabit Edition/g" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-gigabit.dts

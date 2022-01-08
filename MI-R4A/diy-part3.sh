3574838f427fb54b074bfa856b123dfa90e28a3e
WillowSauceR <63277109+WillowSauceR@users.noreply.github.com>
2022-01-08 17:35:44 +0800

    Update diy-part3.sh

--- MI-R4A/diy-part3.sh
+++ MI-R4A/diy-part3.sh
@@ -10,5 +10,7 @@
 # Description: OpenWrt DIY script part 3 (Before Update feeds)
 #
 
-mv MI-R4A/favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/favicon.ico  
+mv MI-R4A/favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/favicon.ico
+mv MI-R4A/2019-10-07_13.36-540p.jpg openwrt/feeds/kenzo/luci-theme-ifit/files/htdocs/images/main_bg.jpg
+rm openwrt/feeds/kenzo/luci-theme-ifit/files/htdocs/images/main_bg_5k.jpg
 mv MI-R4A/banner openwrt/package/base-files/files/etc/banner

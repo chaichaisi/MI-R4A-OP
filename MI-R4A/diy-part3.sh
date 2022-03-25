7e3cd81729f684e946c0f748ea0de3fd68ce6928
WillowSauceR <63277109+WillowSauceR@users.noreply.github.com>
2022-03-25 23:39:03 +0800

    modify banner

--- MI-R4A/diy-part3.sh
+++ MI-R4A/diy-part3.sh
@@ -11,4 +11,4 @@
 #
 
 #mv MI-R4A/favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/favicon.ico
-#mv MI-R4A/banner openwrt/package/base-files/files/etc/banner
+mv MI-R4A/banner openwrt/package/base-files/files/etc/banner

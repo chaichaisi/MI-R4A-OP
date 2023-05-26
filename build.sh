e5afd6dd25d424f9b7d67976090de2f128252299
ExtcanaRy <WillowSauceR@outlook.com>
2023-05-26 16:33:56 +0800

    add curl & change build config

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-REPO_URL=https://github.com/coolsnowwolf/lede
+REPO_URL=https://hub.fgit.ml/coolsnowwolf/lede
 REPO_BRANCH=master
 FEEDS_CONF=$PWD/R4A.conf.default
 CONFIG_FILE=$PWD/R4A.config
@@ -10,15 +10,17 @@ TZ=Asia/Shanghai
 
 sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
 sudo -E apt-get -qq update
-sudo -E apt-get -qq install zip
+sudo -E apt-get -qq install zip curl
+echo installing depends
 sudo -E apt-get -qq install $(curl -fsSL git.io/depends-ubuntu-2004)
+echo finished installing depends
 sudo -E apt-get -qq autoremove --purge
 sudo -E apt-get -qq clean
 sudo timedatectl set-timezone "$TZ"
 
 git clone $REPO_URL -b $REPO_BRANCH openwrt
 
-[ -e $FEEDS_CONF ] && mv $FEEDS_CONF openwrt/feeds.conf.default
+[ -e $FEEDS_CONF ] && cp -r $FEEDS_CONF openwrt/feeds.conf.default
 chmod +x *.sh
 cd openwrt
 $DIY_P1_SH
@@ -26,13 +28,13 @@ $DIY_P1_SH
 
 ./scripts/feeds install -a
 
-[ -e $REPO_PATH/files ] && mv $REPO_PATH/files files
+[ -e $REPO_PATH/files ] && cp -r $REPO_PATH/files files
 [ -e $CONFIG_FILE ] && cp $CONFIG_FILE .config
 #sed -i s/.*=n//g .config
 sed -i s/\#.*//g .config
 $DIY_P2_SH
 # $DIY_P3_SH
-mv $REPO_PATH/banner package/base-files/files/etc/banner
+cp -r $REPO_PATH/banner package/base-files/files/etc/banner
 
 make defconfig
 make download -j8
